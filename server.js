6c5714e71e1bd0bb6ad8386c6dd79574bdf4f97b
Davetto <joshrgarb@gmail.com>
2022-02-25 14:33:49 -0600

    my-commit

--- server.js
+++ server.js
@@ -17,20 +17,37 @@ var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
-// API Requests
-app.get('/api/:date_string', function (req, res) {
-  const { date_string } = req.params;
+// returns current timestamp, if the date_string is empty
+app.get('/api', (req, res) => {
+  res.json(
+    {
+      "unix": new Date().getTime(),
+      "utc": new Date().toUTCString()
+    }
+  );
+});
+
+// request
+app.get('/api/:date_string', (req, res) => {
+  const {date_string} = req.params;
   let date = new Date(date_string);
 
   if (date.toString() === 'Invalid Date') {
     date = new Date(parseInt(date_string));
-    res.json({
-      error: 'Invalid Date'
-    });
-  } else {
-    res.json({
-      unix: date.getTime(),
-      utc: date.toUTCString()
-    })
+  }
+  if (date.toString() === 'Invalid Date') {
+    return res.json(
+      {
+        "error": "Invalid Date"
+      }
+    );
+  }
+  else {
+    return res.json(
+      {
+        "unix": date.getTime(),
+        "utc": date.toUTCString()
+      }
+    );
   }
 });
